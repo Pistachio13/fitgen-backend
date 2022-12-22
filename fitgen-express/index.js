16c3875b653281c0199ae3a08509e51a3506980a
Krit357 <114389319+Krit357@users.noreply.github.com>
2022-12-22 18:33:03 +0700

    add ,

--- fitgen-express/index.js
+++ fitgen-express/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 
 app.use(cors({
   // origin: 'https://jsd-fitgen-frontend.vercel.app', 
-  credentials: true 
+  credentials: true,
 }));
 
 // app.use('/users', userRoutes)
